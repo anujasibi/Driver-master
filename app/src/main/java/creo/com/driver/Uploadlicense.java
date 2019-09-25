5d9cf551a69806dbc671c5e0ccf0b3a1154b9108
pm <pm@creopedia.com>
2019-09-25 18:05:47 +0530

    commit

---
+++ app/src/main/java/creo/com/driver/Uploadlicense.java
@@ -0,0 +1,14 @@
+package creo.com.driver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Uploadlicense extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uploadlicense);
+    }
+}
